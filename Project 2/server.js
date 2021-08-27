116fe90bf19e9d64dc692066f60a22eda6ff2774
James-HPJ <85088804+James-HPJ@users.noreply.github.com>
2021-08-27 14:49:06 +0800

    Project 2 file setup

---
+++ Project 2/server.js
@@ -0,0 +1,33 @@
+const express = require('express')
+const session = require("express-session")
+const mongoose = require('mongoose')
+const methodOverride = require('method-override')
+
+const mongoURI = "mongodb://localhost:27017/snakesdb"
+const dbConnection = mongoose.connection
+
+mongoose.connect(mongoURI, {useNewUrlParser: true, useUnifiedTopology: true})
+
+dbConnection.on("connected", () => console.log(" Database is connected"))
+dbConnection.on("error", (err) => console.log(err.message))
+dbConnection.on("disconnected", () => console.log("Database is disconnected"))
+
+
+const app = express()
+
+app.use(express.static("public"))
+app.use(express.urlencoded({ extended: true }))
+app.use(methodOverride("_method"))
+
+
+
+
+
+const server = app.listen(3000)
+
+process.on("SIGTERM", () => {
+  console.log("My process is exiting")
+  server.close(() => {
+    dbConnection.close()
+  })
+})
