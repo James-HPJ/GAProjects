e1a6b42ec14ff41b98a5a9bb55a1fb90d949d1f5
James-HPJ <85088804+James-HPJ@users.noreply.github.com>
2021-08-27 16:05:23 +0800

    snakes/users schema updated

---
+++ Project 2/models/snakes.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose')
+
+const {Schema} = mongoose
+
+//Schema for point(location) on maps for nesting into main schema
+const pointSchema = new Schema({
+    type: {
+      type: String,
+      enum: ['Point'],
+      required: true
+    },
+    coordinates: {
+      type: [Number],
+      required: true
+    }
+  })
+
+const snakeSchema = new Schema (
+    {
+        commonName: { type: String, required: true },
+        scientificName: String,
+        img: String,
+        dateFound: { type: Date, default: new Date() },
+        description: {type: String, required: true },
+        location: {type: pointSchema, index: '2dsphere'},
+    },
+
+    {
+        timestamps: true,
+    }
+)
+
+const Snake = mongoose.model('Snake', snakeSchema)
+
+module.exports = Snake
